9390cfc8eea8a6097c116f5a05e5967a641c5447
Francisco Solís <fsolisgormaz@uc.cl>
2022-04-01 12:09:13 -0300

    Cambios en main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
-
+from classes import Link, ArchivoAudio, Recognition, Texto
 
 
 def main():
-    pass
+    url = input("Ingrese la url del video que quiere utilizar")
+    opcion = int(input(("""Indique que quiere hacer: 1)Descargar el audio de un video\n2)Extraer el audio de un video(.wav)
+    y dividirlo en partes iguales\n3)Descargar video, extraer audio y pasarlo a archivo Word\n4)Transcribir archivos de audio""")))
+    
+    
+    if opcion == 1:
+        link = Link(url)
+        link.descargar_audio()
+    
+    elif opcion == 2:
+        audio = ArchivoAudio("Audio.wav")
+        audio.division_partes_iguales()
+    
+    elif opcion == 3:
+        link = Link(url)
+        link.descargar_audio()
+        link.conversion_formato_video_a_audio_wav()
 
 
 
